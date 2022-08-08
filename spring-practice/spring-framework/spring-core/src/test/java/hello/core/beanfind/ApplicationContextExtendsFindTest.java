5c2ecd078de92f415fd01e042173986833595cca
jbpark-in-seoul <jbparkinseoul@gmail.com>
2022-08-08 09:56:49 +0900

    add test code: spring bean

---
+++ spring-practice/spring-framework/spring-core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -0,0 +1,53 @@
+package hello.core.beanfind;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextExtendsFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+    @Configuration
+    static class TestConfig() {
+
+        @Test
+        @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 중복 오류가 발생한다.")
+        void findBeanByParentTypeDuplicate() {
+            DiscountPolicy bean = ac.getBean(DiscountPolicy.class);
+            assertThrows(NoUniqueBeanDefinitionException.class,
+                    () -> ac.getBean(DiscountPolicy.class));
+        }
+
+        @Test
+        @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 빈 이름을 지정하면 된다.")
+        void findBeanByParentTypeBeanName() {
+            DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscouintPolicy", DiscountPolicy.class);
+            assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
+        }
+
+        @Test
+        @DisplayName("특정 하위 타입으로 조회")
+        void findBeanBySubType() {
+            RateDiscountPoilicy bean = ac.getBean(RateDiscountPolicy.class);
+            assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
+        }
+
+
+        @Bean
+        public DiscountPolicy rateDiscountPolicy() {
+            return new RateDiscountPolicy();
+        }
+
+        @Bean
+        public DiscountPolicy fixDiscountPolicy() {
+            return new fixDiscountPolicy();
+        }
+    }
+}
