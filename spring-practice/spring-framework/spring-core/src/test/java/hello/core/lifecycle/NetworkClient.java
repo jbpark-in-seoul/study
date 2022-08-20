c0c7c6247a2ae44c3791ca5f51a36ce58e1e0cfa
jbpark-in-seoul <jbparkinseoul@gmail.com>
2022-08-20 14:36:11 +0900

    add bean lifecycle example

--- spring-practice/spring-framework/spring-core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ spring-practice/spring-framework/spring-core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -17,7 +17,10 @@ import javax.annotation.PreDestroy;
  * ### 스프링 빈 생명주기 지원 콜백
  *  - 인터페이스(InitializingBean, DisposableBean)
  *    + 스프링 종속적으로 권고되지 않음.
- *  - 설정 정보에 초기화 메서드, 종료 메서드 지정
+ *  - @Bean(initMethod = "init", destroyMethod = "close"): 설정 정보에 초기화 메서드, 종료 메서드 지정
+ *    + 스프링 빈이나 스프링 코드에 비의존적이다.
+ *    + 코드가 아닌 설정 정보를 사용하기 때문에 외부 라이브러리에도 초기화, 종료 메서드를 적용할 수 있다.
+ *    + destroyMethod 는 기본값이 '(inffered)'(=추론)으로 close, shutdown 이름의 메서드를 자동 호출해준다.
  *  - @PostConstruct, @PreDestory 애노테이션 지원 (권장 방식)
  *    + 스프링에서 권장되는 방식
  */
