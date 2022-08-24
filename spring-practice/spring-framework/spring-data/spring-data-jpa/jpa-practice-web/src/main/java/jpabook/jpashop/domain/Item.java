415fca0ef193727c7cfc4fb7b69ca5a88888b3e0
jbpark-in-seoul <jbparkinseoul@gmail.com>
2022-08-24 20:15:06 +0900

    feat(item) add code for service and repository

--- spring-practice/spring-framework/spring-data/spring-data-jpa/jpa-practice-web/src/main/java/jpabook/jpashop/domain/Item.java
+++ spring-practice/spring-framework/spring-data/spring-data-jpa/jpa-practice-web/src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,10 +20,29 @@ public abstract class Item {
     private Long id;
 
     private String name;
-
     private int price;
     private int stockQuantity;
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /**
+     * stock 증가
+     * @param quantity
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity = quantity;
+    }
+
+    /**
+     * stock 감소
+     * @param quantity
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
