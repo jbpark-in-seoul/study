bc634fc19f4c61748782befd046cd1df31e5b525
jbpark-in-seoul <jbparkinseoul@gmail.com>
2022-09-05 14:09:23 +0900

    add example code for JPA relationship mapping

--- spring-practice/spring-framework/spring-data/spring-data-jpa/jpa-practice-basic/jpabook/src/main/java/jpabook/jpashop/domain/Member.java
+++ spring-practice/spring-framework/spring-data/spring-data-jpa/jpa-practice-basic/jpabook/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -8,21 +10,13 @@ public class Member {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "MEMBER_ID")
     private Long id;
-
-    @Column(name = "USERNAME")
     private String name;
+    private String city;
+    private String street;
+    private String zipcode;
 
-    @ManyToOne
-    @JoinColumn(name = "TEAM_ID")
-    private Team team;
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>(); // example
 
     public Long getId() {
         return id;
@@ -40,4 +34,35 @@ public class Member {
         this.name = name;
     }
 
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
 }
